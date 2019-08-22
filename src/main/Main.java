5f3ad9b70ed6e170c923e3109952cbd6fc133c58
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2019-08-22 15:33:14 +0530

    final commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String username = scanner.nextLine().trim();
         System.out.println("Welcome," + username + "!");
+        System.out.println("ho gaya!!!");
         scanner.close();
 
 
