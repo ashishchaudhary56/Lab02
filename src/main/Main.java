79cf4d286974694321df5fb0b1224ecf64e2c339
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2019-08-22 15:16:40 +0530

    add output to the screen

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String username = scanner.nextLine().trim();
+        System.out.println("Welcome," + username + "!");
         scanner.close();
 
 
