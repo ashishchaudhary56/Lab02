ad78d661327f28729c9c919f6ac821052b38a8ba
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2019-08-22 15:13:44 +0530

    create object of scanner class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,16 @@
 package main;
 
+import java.util.Scanner;
+
 /**
  * @author ashish chaudhary
  * @version 1.0
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String username = "";
+        scanner.close();
 
 
     }
